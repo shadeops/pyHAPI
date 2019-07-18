30c8ed02263dc3ebf5aefa77ea52e74c4a941a8a
jim price <shadeops@gmail.com>
2019-07-17 19:29:25 -0700

    Placeholder exception for missing libHAPIL

--- hapi/_library.py
+++ hapi/_library.py
@@ -1,8 +1,11 @@
 import ctypes as _ctypes
 from platform import system as _system_platform
 
-if _system_platform() == 'Windows':
-    libHAPI = _ctypes.cdll.LoadLibrary('libHAPIL.lib')
-else:
-    libHAPI = _ctypes.cdll.LoadLibrary('libHAPIL.so')
+try:
+    if _system_platform() == 'Windows':
+        libHAPI = _ctypes.cdll.LoadLibrary('libHAPIL.lib')
+    else:
+        libHAPI = _ctypes.cdll.LoadLibrary('libHAPIL.so')
+except OSError:
+    raise OSError('Unable to find libHAPIL library')
 
